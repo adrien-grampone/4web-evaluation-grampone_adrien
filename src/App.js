38570398425182ee627494addc1dd6fc308b034d
Adrien Grampone <adriengrampone@MacBook-Pro-de-Adrien.local>
2019-12-13 11:27:56 +0100

    feat(login): init form

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from "./layout/Header/Header.js";
 import './App.css';
 import Login from './Login/Login.js';
 import Dashboard from './Dashboard/Dashboard.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Login />
-      <Dashboard />
+      <Router>
+        <Link to="/login">Login</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Router>
+
     </div>
   );
 }
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import './Login.css';
 
-const Login = () => <div class="login">Login</div>
+
+const Login = () =>
+
+
+    <div class="container">
+        <div class="main">
+
+            <form id="form_id" method="post" name="myform">
+                <label for="username">Nom d'utilisateur (test) :</label>
+                <input type="text" name="username" id="username" />
+                <label for="password">Mot de passe (Test12345@) :</label>
+                <input type="password" name="password" id="password" />
+                <h3> Password strength : <span class="password-strength__value">0</span></h3>
+                <div id="myProgress">
+                    <div id="myBar"></div>
+                </div>
+
+
+
+                <input type="button" value="Connexion" id="submit" onclick="validate()" />
+            </form>
+
+        </div>
+    </div>;
 
 export default Login;
